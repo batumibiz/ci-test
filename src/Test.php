443b4974216f5d70f3f927834483dcd575980a7b
Oleg Kasyanov <dev@mobicms.net>
2020-04-29 18:26:51 +0400

    Initial commit

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Test\Other\MyClass;
+
+class Test
+{
+    /**
+     * @return bool
+     */
+    public function foo()
+    {
+        return false;
+    }
+}
