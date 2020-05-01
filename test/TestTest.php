328956e1fdbb5ff323af74e101222af4aab5de13
Oleg Kasyanov <dev@mobicms.net>
2020-05-01 19:55:50 +0400

    Добавлен тест класса Test\Test

---
+++ test/TestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Test\Test;
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+    public function testFoo()
+    {
+        $instance = new Test();
+        $this->assertIsString($instance->foo());
+    }
+}
